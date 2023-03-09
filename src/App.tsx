a1c8541963775bfa1dc9ff3afba7b4cfbcc9d0ba
abdul35 <abdul355428@bk.ru>
2023-03-09 22:46:11 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container mx-auto">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Guilds.tsx
@@ -0,0 +1,13 @@
+
+
+function Guilds(): JSX.Element {
+    return ( 
+        <div className="guilds">
+            <div className="tree">
+        
+            </div>
+        </div>
+     );
+}
+
+export default Guilds;
